7e29df9bd9c2ef721069ea1749a5a398c695d9dd
Natalia <s215144@dtu.dk>
2023-04-26 12:38:27 +0200

    Co-authored-by: maslionok <maslionok@users.noreply.github.com> Co-authored-by: OlivierJar <OlivierJar@users.noreply.github.com>

--- roborally-master/src/main/java/dtu/view/ProgrammingPhaseScene.java
+++ roborally-master/src/main/java/dtu/view/ProgrammingPhaseScene.java
@@ -10,11 +10,13 @@ import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import dtu.logic.models.Position;
 import dtu.logic.models.RobotColor;
 import dtu.logic.models.Cards.Deck;
 import dtu.logic.models.Player.Player;
 import dtu.logic.models.Robot.Robot;
+import io.cucumber.plugin.event.EventHandler;
 import javafx.scene.paint.Color;
 
     
@@ -36,6 +38,10 @@ public class ProgrammingPhaseScene extends Scene {
     VBox leftBox3;
     VBox leftBox4;
     VBox leftBox5;
+
+    Rectangle frame;
+
+    ImageView imageView1;
 
     
     public ProgrammingPhaseScene(Controller c) {
@@ -74,10 +80,10 @@ public class ProgrammingPhaseScene extends Scene {
      
            
         
-        try{
+        // try{
         // Card1
         Image stream = new Image (ProgrammingPhaseScene.class.getClassLoader().getResourceAsStream(player1.getHand().get(0).getImage())); //new FileInputStream("file:src/main/resources/Cards/left_turn.png")); 
-        ImageView imageView1 = new ImageView(stream);  
+        this.imageView1=new ImageView(stream); //new ImageView(stream);  
         imageView1.setFitHeight(150);
         imageView1.setFitWidth(100);
         imageView1.setId("Card-0");
@@ -88,52 +94,59 @@ public class ProgrammingPhaseScene extends Scene {
         imageView2.setFitHeight(150);
         imageView2.setFitWidth(100);
         
-        
-            mainGrid.add(imageView1, 3, 0, 1, 1);
-            mainGrid.add(imageView2, 3,1,1,1);
-        }
-        catch(Exception e){System.out.println(e);}
-        
-      
-        
-    
+        Image stream3 = new Image (ProgrammingPhaseScene.class.getClassLoader().getResourceAsStream(player1.getHand().get(2).getImage())); //new FileInputStream("file:src/main/resources/Cards/left_turn.png"));
+        ImageView imageView3 = new ImageView(stream3);
+        imageView3.setFitHeight(150);
+        imageView3.setFitWidth(100);
 
+        
+        mainGrid.add(imageView1, 3, 0, 1, 1);
+        mainGrid.add(imageView2, 3,1,1,1);
+        mainGrid.add(imageView3, 3,2,1,1);
+            
+        // }
+        // catch(Exception e){System.out.println(e);}
+        
         mainGrid.getChildren().forEach(this::makeDraggable);
 
+
         //Frames for left boxes
-        Rectangle frame = new Rectangle(100,150);
+        this.frame = new Rectangle(100,150);
         frame.setStroke(Color.BLACK);
         frame.setFill(null);
-        // this.leftBox1 = new VBox(frame);
-        mainGrid.add(frame, 0, 0, 1, 1);
+        this.leftBox1 = new VBox(frame);
+        mainGrid.add(leftBox1, 0, 0, 1, 1);
 
         // BoundingBox leftBox1 = new BoundingBox(0, 0, 100, 150);
         
         Rectangle frame2 = new Rectangle(100,150);
         frame2.setStroke(Color.RED);
         frame2.setFill(null);
-        // this.leftBox2 = new VBox(frame2);
-        mainGrid.add(frame2, 0, 1, 1, 1); 
+        this.leftBox2 = new VBox(frame2);
+        mainGrid.add(leftBox2, 0, 1, 1, 1); 
 
         Rectangle frame3 = new Rectangle(100,150);
         frame3.setStroke(Color.BLUE);
         frame3.setFill(null);
-        // this.leftBox3 = new VBox(frame3);
-        mainGrid.add(frame3, 0, 2, 1, 1);
+        this.leftBox3 = new VBox(frame3);
+        mainGrid.add(leftBox3, 0, 2, 1, 1);
 
         Rectangle frame4 = new Rectangle(100,150);
         frame4.setStroke(Color.GREEN);
         frame4.setFill(null);
-        // this.leftBox4 = new VBox(frame4);
-        mainGrid.add(frame4, 0, 3, 1, 1);
+        this.leftBox4 = new VBox(frame4);
+        mainGrid.add(leftBox4, 0, 3, 1, 1);
 
         Rectangle frame5 = new Rectangle(100,150);
         frame5.setStroke(Color.PURPLE);
         frame5.setFill(null);
-        // this.leftBox5 = new VBox(frame5);
-        mainGrid.add(frame5, 0, 4, 1, 1);
+        this.leftBox5 = new VBox();
+        leftBox5.getChildren().add(frame5);
+        // System.out.println("CHECKING LEFTBOX5");
+        // System.out.println(leftBox5.getChildren());
+        mainGrid.add(leftBox5, 0, 4, 1, 1);
     }
-
+    
 
     private double startX;
     private double startY;
@@ -150,82 +163,76 @@ public class ProgrammingPhaseScene extends Scene {
         });
 
         node.setOnMouseReleased(e->{
-            //go through all boxes and check if we released the mouse inside one of them
-                // mainGrid.getChildren().get(0);
-                
-                System.out.println(mainGrid.getChildren().toString());
-                if (mainGrid.getChildren().get(0).contains(e.getSceneX(), e.getSceneY())){
-                    System.out.println("mainGrid get(1) box toggle");
-                    // System.out.println(node.getId());
-                    System.out.println(mainGrid.getChildren().toString());
-                }
-                if (mainGrid.getChildren().get(1).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(1) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(2).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(2) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(3).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(3) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(4).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(4) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(5).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(5) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(6).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(6) box toggle");
-                    // System.out.println(node.getId());
-                }
-                if (mainGrid.getChildren().get(7).contains(e.getSceneX(), e.getSceneY())){
-                    // System.out.println("mainGrid get(7) box toggle");
-                    // System.out.println(node.getId());
-                }
+            double mouseX = e.getSceneX();
+            double mouseY = e.getSceneY();
 
-
-
-                // if (leftBox1.contains(e.getSceneX(), e.getSceneY())){
-                //     System.out.println("entered if");
-                //     System.out.println(node.getId());
-                //     leftBox1.getChildren().add(node);
+            //if (mouseX>=0 & mouseX<=100 ){
+                // box1
+                // if (mouseY>=0 & mouseY<150){
+                //     System.out.println("in box 1");
+                //     System.out.println("Checking kiddos before:");
                 //     System.out.println(leftBox1.getChildren());
-                //     // leftBox1.removeChild(node);
-                //     // leftBox1.getChildren().removeAll(leftBox1.getChildren());
-                // }
+                //     leftBox1.getChildren().get(0).getChildren().add(node);
+                //     System.out.println("Checking kiddos after:");
+                //     System.out.println(leftBox1.getChildren());
 
-                // //if we did, then we set the card to the center of the box
-                // if (leftBox2.contains(e.getSceneX(), e.getSceneY())){
-                //     System.out.println("entered if second one");
-                //     System.out.println(node.getId());
+                // }    
 
-                // }
-                
-                else {
-                    // node.setTranslateX(-);
-                    node.setLayoutY(startY);
-                    System.out.print(startX);
-                    //System.out.print(startY);
+                if (leftBox1.getBoundsInParent().contains(e.getSceneX(), e.getSceneY())){
+                    System.out.println("WE are now in box 1");
                 }
-        
-        });
-        // node.setOnMouseClicked(e->{
-        //     if (leftBox1.contains(e.getSceneX(), e.getSceneY())){
-        //         leftBox1.getChildren().clear();
-        //   }
-        // // node.setOnMouseReleased();   
+                            
+                if (leftBox2.getBoundsInParent().contains(e.getSceneX(), e.getSceneY())){
+                                System.out.println("we are now in box 2");
+                            }
+
+                // }
+                // if (mouseY>=150 & mouseY<300){
+                //     System.out.println("in box 2");
+                     
+
+                // }         
+                // if (mouseY>=300 & mouseY<450){
+                //     System.out.println("in box 3");
+                     
+
+                // }
+                // if (mouseY>=450 & mouseY<600){
+                //     System.out.println("in box 4");
+                     
+
+                // }
+                // if (mouseY>=600 & mouseY<750){
+                //     System.out.println("in box 5");
+                     
+
+                // }
             
-        //  });
-    }
-}
+            
+    
+        
+         });
+
+
+    
+         
+         }
+
+    };
+        
+
+    //     // node.setOnMouseClicked(e->{
+    //     //     if (leftBox1.contains(e.getSceneX(), e.getSceneY())){
+    //     //         leftBox1.getChildren().clear();
+    //     //   }
+    //     // // node.setOnMouseReleased();   
+            
+    //     //  });
+    // }
 
 
 
+    
 
 //HOW TO GET NODE FROM GRIDPANE
 
