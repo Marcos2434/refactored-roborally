7ecbf1fb554fbcd10692bf51e1dc1a847a911f96
Natalia <s215144@dtu.dk>
2023-04-27 16:23:53 +0200

    d

--- roborally-master/src/main/java/dtu/view/MenuScene.java
+++ roborally-master/src/main/java/dtu/view/MenuScene.java
@@ -32,9 +32,9 @@ public class MenuScene extends Scene {
     private void initialize() {
         // BorderPane menu = new BorderPane();
 
-		Image background = new Image (("playermat/menubackground1.png"));
-        BackgroundImage background2 = new BackgroundImage(background, null, null, null, null);
-        Background background3 = new Background(background2);
+		// Image background = new Image (("playermat/menubackground.png"));
+        // BackgroundImage background2 = new BackgroundImage(background, null, null, null, null);
+        // Background background3 = new Background(background2);
         
 
         
@@ -42,106 +42,82 @@ public class MenuScene extends Scene {
         //Set backround to the whole scene 
         
         BorderPane menu = (BorderPane) this.getRoot();
-		menu.setBackground(background3);
+		// menu.setBackground(background3);
 		menu.setPrefSize(500, 500);
 
 		// RED
-		//Text r = new Text("RED");
+		Text r = new Text("RED");
 		TextField red= new TextField();
 		red.setPromptText("Player Name");
-		red.setStyle("-fx-background-color: #ff3333;");
 		CheckBox CheckRed = new CheckBox("Is red playing?");
-		CheckRed.setStyle("-fx-Text-fill: #ffffff;");
 		CheckBox AIRed = new CheckBox("Is AI?");
-		AIRed.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxRED = new VBox(red,CheckRed,AIRed);    
+		VBox vboxRED = new VBox(r,red,CheckRed,AIRed);
 		vboxRED.setPrefSize(125, 200);
 		
 		//BLUE
-		//Text b = new Text("BLUE");
+		Text b = new Text("BLUE");
 		TextField blue= new TextField();
 		blue.setPromptText("Player Name");
 		CheckBox CheckBlue = new CheckBox("Is blue playing?");
-		CheckBlue.setStyle("-fx-Text-fill: #ffffff;");
-		blue.setStyle("-fx-background-color: #80e5ff;");
 		CheckBox AIBlue = new CheckBox("Is AI?");
-		AIBlue.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxBLUE = new VBox(blue,CheckBlue,AIBlue);
+		VBox vboxBLUE = new VBox(b,blue,CheckBlue,AIBlue);
 		vboxBLUE.setPrefSize(125, 200);
 	
 		//GREEN
-		//Text g = new Text("GREEN");
+		Text g = new Text("GREEN");
 		TextField green= new TextField();
 		green.setPromptText("Player Name");
-		green.setStyle("-fx-background-color: #66ff33;");
 		CheckBox CheckGreen = new CheckBox("Is green playing?");
-		CheckGreen.setStyle("-fx-Text-fill: #ffffff;");
 		CheckBox AIGreen = new CheckBox("Is AI?");
-		AIGreen.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxGREEN = new VBox(green,CheckGreen,AIGreen);
+		VBox vboxGREEN = new VBox(g,green,CheckGreen,AIGreen);
 		vboxGREEN.setPrefSize(125, 200);
 
 		//YELLOW
-		//Text y = new Text("YELLOW");
+		Text y = new Text("YELLOW");
 		TextField yellow= new TextField();
 		yellow.setPromptText("Player Name");
 		CheckBox CheckYellow = new CheckBox("Is yellow playing?");
-		CheckYellow.setStyle("-fx-Text-fill: #ffffff;");
-		yellow.setStyle("-fx-background-color: #ffff33;");
 		CheckBox AIYellow = new CheckBox("Is AI?");
-		AIYellow.setStyle("-fx-Text-fill: #ffffff;");//
-		VBox vboxYELLOW = new VBox(yellow,CheckYellow,AIYellow);
+		VBox vboxYELLOW = new VBox(y,yellow,CheckYellow,AIYellow);
 		vboxYELLOW.setPrefSize(125, 200);
 		
 		
 		HBox RGB1 = new HBox(vboxRED,vboxBLUE, vboxGREEN, vboxYELLOW);
 
 		//ORANGE
-		//Text o = new Text("ORANGE");
+		Text o = new Text("ORANGE");
 		TextField orange= new TextField();
 		orange.setPromptText("Player Name");
 		CheckBox CheckOrange = new CheckBox("Is orange playing?");
-		CheckOrange.setStyle("-fx-Text-fill: #ffffff;");
-		orange.setStyle("-fx-background-color: #ff8533;");
 		CheckBox AIOrange = new CheckBox("Is AI?");
-		AIOrange.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxORANGE = new VBox(orange,CheckOrange,AIOrange);
+		VBox vboxORANGE = new VBox(o,orange,CheckOrange,AIOrange);
 		vboxORANGE.setPrefSize(125, 200);
 
 		//PURPLE
-		//Text p = new Text("PURPLE");
+		Text p = new Text("PURPLE");
 		TextField purple= new TextField();
 		purple.setPromptText("Player Name");
-		purple.setStyle("-fx-background-color: #9933ff;");
 		CheckBox CheckPurple = new CheckBox("Is purple playing?");
-		CheckPurple.setStyle("-fx-Text-fill: #ffffff;");
 		CheckBox AIPurple = new CheckBox("Is AI?");
-		AIPurple.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxPURPLE = new VBox(purple,CheckPurple, AIPurple);
+		VBox vboxPURPLE = new VBox(p,purple,CheckPurple, AIPurple);
 		vboxPURPLE.setPrefSize(125, 200);
 
 		//WHITE
-		//Text w = new Text("WHITE");
+		Text w = new Text("WHITE");
 		TextField white= new TextField();
 		white.setPromptText("Player Name");
-		white.setStyle("-fx-background-color: #ffffff;");
 		CheckBox CheckWhite = new CheckBox("Is white playing?");
-		CheckWhite.setStyle("-fx-Text-fill: #ffffff;");
 		CheckBox AIWhite = new CheckBox("Is AI?");
-		AIWhite.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxWHITE = new VBox(white,CheckWhite,AIWhite);
+		VBox vboxWHITE = new VBox(w,white,CheckWhite,AIWhite);
 		vboxWHITE.setPrefSize(125, 200);
 
 		//BLACK
-		//Text bl = new Text("BLACK");
+		Text bl = new Text("BLACK");
 		TextField black= new TextField();
 		black.setPromptText("Player Name");
-		black.setStyle("-fx-background-color: #262626;");
 		CheckBox CheckBlack = new CheckBox("Is black playing?");
-		CheckBlack.setStyle("-fx-Text-fill: #ffffff;");
 		CheckBox AIBlack = new CheckBox("Is AI?");
-		AIBlack.setStyle("-fx-Text-fill: #ffffff;");
-		VBox vboxBLACK = new VBox(black,CheckBlack,AIBlack);
+		VBox vboxBLACK = new VBox(bl,black,CheckBlack,AIBlack);
 		vboxBLACK.setPrefSize(125, 200);
 
 		HBox RGB2 = new HBox(vboxORANGE,vboxPURPLE, vboxWHITE, vboxBLACK);
@@ -173,46 +149,86 @@ public class MenuScene extends Scene {
 
 						}
 						if (checkarray[i].getText().contains("blue")){
-							if (blue.getText().isEmpty()){
-								blue.setText("BLUE");
+							if (AIBlue.isSelected()){
+								c.createAI(RobotColor.BLUE);
 							}
-							c.createPlayer((RobotColor.BLUE), blue.getText());
+							else{
+								if (blue.getText().isEmpty()){
+									blue.setText("BLUE");
+								}
+								c.createPlayer((RobotColor.BLUE), blue.getText());
+							}
+
 						}
 						if (checkarray[i].getText().contains("green")){
-							if (green.getText().isEmpty()){
-								green.setText("GREEN");
+							if (AIGreen.isSelected()){
+								c.createAI(RobotColor.GREEN);
 							}
-							c.createPlayer((RobotColor.GREEN), green.getText());
+							else{
+								if (green.getText().isEmpty()){
+									green.setText("GREEN");
+								}
+								c.createPlayer((RobotColor.GREEN), green.getText());
+							}
+
 						}
 						if (checkarray[i].getText().contains("yellow")){
-							if (yellow.getText().isEmpty()){
-								yellow.setText("YELLOW");
+							if (AIYellow.isSelected()){
+								c.createAI(RobotColor.YELLOW);
 							}
-							c.createPlayer((RobotColor.YELLOW), yellow.getText());
+							else{
+								if (yellow.getText().isEmpty()){
+									yellow.setText("YELLOW");
+								}
+								c.createPlayer((RobotColor.YELLOW), yellow.getText());
+							}
+
 						}
 						if (checkarray[i].getText().contains("orange")){
-							if (orange.getText().isEmpty()){
-								orange.setText("ORANGE");
+							if (AIOrange.isSelected()){
+								c.createAI(RobotColor.ORANGE);;
+								
 							}
-							c.createPlayer((RobotColor.ORANGE), orange.getText());
+							else{
+								if (orange.getText().isEmpty()){
+									orange.setText("ORANGE");
+								}
+								c.createPlayer((RobotColor.ORANGE), orange.getText());
+							}
+
 						}
 						if (checkarray[i].getText().contains("purple")){
-							if (purple.getText().isEmpty()){
-								purple.setText("PURPLE");
+							if (AIPurple.isSelected()){
+								c.createAI(RobotColor.PURPLE);
 							}
-							c.createPlayer((RobotColor.PURPLE), purple.getText());
+							else{
+								if (purple.getText().isEmpty()){
+								purple.setText("PURPLE");
+								}
+								c.createPlayer((RobotColor.PURPLE), purple.getText());
+							}
 						}
 						if (checkarray[i].getText().contains("white")){
-							if (white.getText().isEmpty()){
-								white.setText("WHITE");
+							if (AIWhite.isSelected()){
+								c.createAI(RobotColor.WHITE);
 							}
-							c.createPlayer((RobotColor.WHITE), white.getText());
+							else{
+								if (white.getText().isEmpty()){
+									white.setText("WHITE");
+								}
+								c.createPlayer((RobotColor.WHITE), white.getText());
+							}
 						}
 						if (checkarray[i].getText().contains("black")){
-							if (black.getText().isEmpty()){
-								black.setText("BLACK");
+							if (AIBlack.isSelected()){
+								c.createAI(RobotColor.BLACK);
 							}
-							c.createPlayer((RobotColor.BLACK), black.getText());
+							else{
+								if (black.getText().isEmpty()){
+									black.setText("BLACK");
+								}
+								c.createPlayer((RobotColor.BLACK), black.getText());
+							}
 						}
 					}
 				}
