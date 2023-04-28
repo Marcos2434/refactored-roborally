49dc0a20eb9f26f3caecd23dd7ffd221f2130088
Casper Sonne <csonne8@gmail.com>
2023-04-28 14:32:31 +0200

    zc

--- roborally-master/src/main/java/dtu/logic/models/Cards/MovementCards/Backwards.java
+++ roborally-master/src/main/java/dtu/logic/models/Cards/MovementCards/Backwards.java
@@ -1,5 +1,5 @@
 package dtu.logic.models.Cards.MovementCards;
-import dtu.logic.models.Board.Board;
+
 import dtu.logic.models.Board.BoardController;
 import dtu.logic.models.Cards.ProgramCard;
 import dtu.logic.models.Robot.Robot;
